f386d3c40dae7eb92e2b821468d3835fac6e1f15
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-03-22 20:39:27 -0500

    updates work section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -7,45 +7,68 @@ const Work: React.FC = () => {
     {
       jobTitle: "Senior Software Engineer",
       company: "Critical Mass",
-      startDate: "September 2021",
+      startDate: "May 2023",
       endDate: "Present",
-      description: [],
+      description: [
+        "Architect and strategize innovative solutions to intricate challenges, spearheading the development of cutting-edge features and products",
+        "Craft modern, high-performance, and maintainable code across a diverse portfolio of client and internal projects, adhering to best practices and industry standards",
+        "Collaborate and effectively communicate with multi-disciplinary teams comprising engineers, designers, producers, and clients, providing technical expertise and ensuring the timely delivery of exceptional products",
+      ],
     },
     {
       jobTitle: "Software Engineer",
-      company: "ABC Company",
-      startDate: "January 2020",
-      endDate: "September 2021",
-      description: ["Implemented new features", "Improved performance"],
+      company: "Critical Mass",
+      startDate: "March 2022",
+      endDate: "May 2023",
+      description: [
+        "Develop and implement modern, performant, and maintainable code for a wide range of client and internal projects, adhering to best practices and industry standards",
+        "Demonstrate proficiency in multiple languages, platforms, frameworks, and content management systems, such as JavaScript, TypeScript, React, Adobe Experience Manager, and Salesforce",
+        "Liaise with multi-disciplinary teams of engineers, designers, producers, and clients, offering technical guidance and ensuring the delivery of top-quality products that exceed expectations",
+      ],
     },
     {
       jobTitle: "Software Engineer Intern",
-      company: "XYZ Company",
-      startDate: "June 2019",
-      endDate: "December 2019",
-      description: ["Assisted with testing", "Debugged code"],
+      company: "Critical Mass",
+      startDate: "September 2021",
+      endDate: "March 2022",
+      description: [
+        "Gained valuable industry experience by shadowing seasoned engineers and executing a variety of small-scale independent projects, demonstrating adaptability and eagerness to learn",
+        "Introduced and implemented a range of development tools and automation scripts to streamline workflows and bolster the efficiency of the development team",
+        "Designed, developed, and maintained code for in-house and client websites, utilizing technologies such as HTML, CSS, Less, JavaScript, and jQuery while adhering to best practices",
+      ],
     },
   ];
 
   return (
-    <section className="h-screen mb-8" id="work">
-      <h2>Work Experience</h2>
-      <div className="grid grid-cols-5">
-        <div className="tab-list col-span-2 flex flex-col ">
-          {jobs.map((job, i) => (
-            <button
-              key={i}
-              onClick={() => setCurrentJob(i)}
-              className={currentJob === i ? "active" : ""}
-            >
-              {job.jobTitle}
-            </button>
-          ))}
-        </div>
-        <div className="job-list col-span-3">
-          {jobs.map((job, i) => (
-            <Job {...job} className={`${currentJob === i ? 'active' : 'hidden'}`} key={i} />
-          ))}
+    <section
+      className="h-screen mb-8 max-w-3xl mx-auto flex items-center"
+      id="work"
+    >
+      <div>
+        <h2 className="text-xl mb-8">Work Experience</h2>
+        <div className="flex">
+          <div className="tab-list flex flex-col">
+            {jobs.map((job, i) => (
+              <button
+                key={i}
+                onClick={() => setCurrentJob(i)}
+                className={`${
+                  currentJob === i ? "active " : ""
+                } text-left px-6 text-xs w-max h-12`}
+              >
+                {job.jobTitle}
+              </button>
+            ))}
+          </div>
+          <div className="job-list w-full">
+            {jobs.map((job, i) => (
+              <Job
+                {...job}
+                className={`${currentJob === i ? "active" : "hidden"}`}
+                key={i}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
@@ -67,19 +90,21 @@ const Job: React.FC<JobProps> = ({
   startDate,
   endDate,
   description,
-  className
+  className,
 }) => {
   return (
     <div className={`job ${className}`}>
-      <h3>
-        <span>{jobTitle} </span>@<span>{company}</span>
+      <h3 className="leading-8">
+        <span>{jobTitle} </span>@<a> {company}</a>
       </h3>
-      <span>
+      <p className="text-sm mb-4">
         {startDate} - {endDate}
-      </span>
+      </p>
       <ul>
         {description.map((desc, i) => (
-          <li key={i}>{desc}</li>
+          <li className="mb-4 text-sm" key={i}>
+            {desc}
+          </li>
         ))}
       </ul>
     </div>
